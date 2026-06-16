78baf1f57d5371942fa41bffc2cef5a6b734c525
Corbitt Arfken <caarfken@protonmail.com>
2026-06-16 13:04:52 -0300

    fix http

--- shortener/shorten.js
+++ shortener/shorten.js
@@ -2,4 +2,11 @@ const link = document.getElementById('link');
 const urlParams = new URLSearchParams(window.location.search);
 const l = urlParams.get('l');
 
-link.href = "http://www." + atob(l);
+lengthened = atob(l)
+if (!lengthened.startsWith("http://") && !lengthened.startsWith("https://")) {
+    lengthened = "http://" + lengthened
+}
+else
+{
+    link.href = lengthened
+}
