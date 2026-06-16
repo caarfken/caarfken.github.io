f4adc4cae383708caf5ca3fc2b5220f2f2daa229
Corbitt Arfken <caarfken@protonmail.com>
2026-06-16 12:40:01 -0300

    seperate js

---
+++ shortener/index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('submit');
+const input = document.getElementById('inp');
+const resultdiv = document.getElementById('result')
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    b64result = btoa(inp.value)
+    resultdiv.textContent = b64result
+});
