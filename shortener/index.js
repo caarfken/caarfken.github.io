a00ed94b76f1b9bcbebf3c4418b1c99a53f97097
Corbitt Arfken <caarfken@protonmail.com>
2026-06-16 13:11:57 -0300

    now

--- shortener/index.js
+++ shortener/index.js
@@ -4,6 +4,10 @@ const resultdiv = document.getElementById('result')
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
+    if (inp.value.contains("http") == false)
+    {
+        inp.value = "http://" + inp.value
+    }
     b64result = btoa(inp.value)
     resultdiv.textContent = "caarfken.github.io/shortener/shorten.html?l=" + b64result
 });
--- shortener/shorten.js
+++ shortener/shorten.js
@@ -3,10 +3,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const l = urlParams.get('l');
 
 lengthened = atob(l)
-if (!lengthened.startsWith("http://") && !lengthened.startsWith("https://")) {
-    lengthened = "http://" + lengthened
-}
-else
 {
     link.href = lengthened
 }
