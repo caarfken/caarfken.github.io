5206b6cb5d76d0aca6b877ba19d8f7ddb62f23eb
Corbitt Arfken <caarfken@protonmail.com>
2026-06-28 13:25:14 -0300

    Improve link behaviour; now you can simply right-click the resulting link and copy link

--- shortener/index.js
+++ shortener/index.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('submit');
 const input = document.getElementById('inp');
-const resultdiv = document.getElementById('result')
+const result = document.getElementById('result')
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
@@ -11,5 +11,7 @@ button.addEventListener('click', (event) => {
         : `http://${value}`;
                              // Batshit insane regexes
     const b64result = btoa(normalized);
-    resultdiv.textContent = `caarfken.github.io/shortener/shorten.html?l=${b64result}`;
+	result.style.visibility = "visible";
+    result.href = `http://caarfken.github.io/shortener/shorten.html?l=${b64result}`;
+	result.textContent = `caarfken.github.io/shortener/shorten.html?l=${b64result}`;
 });
